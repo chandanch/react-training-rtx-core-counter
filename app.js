8344305dee8b9cbe890e00c29f9b519f15286146
chandanch <chandan.cec107@gmail.com>
2024-06-05 23:48:57 +0530

    ce

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("RTK Grounds");
+
+const { createAction } = require("@reduxjs/toolkit");
+
+//InitialsState
+const initialsState = {
+  counter: 0,
+};
+
+//Action creator - Action
+const increment = createAction("INCREMENT");
+console.log(increment);
+
+//Reducer
+//Store
